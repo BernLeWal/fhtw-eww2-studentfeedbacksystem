dead58545c4088f6d446d2e5a4435297b7f20165
wallisch <walliscb@technikum-wien.at>
2023-02-24 15:40:11 +0100

    Initial commit

---
+++ src/main/java/at/fhtw/eww/studentfeedbacksystem/StudentFeedbackSystemApplication.java
@@ -0,0 +1,13 @@
+package at.fhtw.eww.studentfeedbacksystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentFeedbackSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentFeedbackSystemApplication.class, args);
+	}
+
+}
